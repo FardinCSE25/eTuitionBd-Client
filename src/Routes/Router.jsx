35b0fd343b980bcfab2aedb19acc8b54383f10bb
Fardin Ahmed <fardin.cse25@gmail.com>
2025-12-06 14:39:41 +0600

    project initialized

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="flex justify-center items-center text-7xl">Hello World</div>,
+  },
+]);
